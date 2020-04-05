bae9f0cce7d4ba184da9da68b5f3be8d15a6bb45
mobinshah <mobinsh_20@yahoo.com>
2020-04-06 01:21:12 +0430

    add:scroll to member box

--- vshare/src/create_room/create_room.js
+++ vshare/src/create_room/create_room.js
@@ -1,14 +1,15 @@
+﻿
 import React, { Component } from 'react'
 import $ from 'jquery';
 import './create_room.css'
 import jQuery from 'jquery'
 import { Cookies } from 'js-cookie'
 class create_room extends Component {
-    
+
 
     componentDidMount() {
-        
-      
+
+
         function getCSRFToken() {
             var cookieValue = null;
             if (document.cookie && document.cookie != '') {
@@ -24,48 +25,48 @@ class create_room extends Component {
             return cookieValue;
         }
         var csrftoken = getCSRFToken();
-      
 
-       // var csrftoken = Cookies.get('csrftoken');
+
+        // var csrftoken = Cookies.get('csrftoken');
         $(document).ready(function () {
 
             var mem = [];
-            var mem2=[];
-            var htmlcode='';
-            var counter=0;
+            var mem2 = [];
+            var htmlcode = '';
+            var counter = 0;
 
-           
+
             $("#homepagebtn").click(function () {
                 window.location.replace("/homepage");
             });
-            
+
             $(".button").click(function () {
-               
-              
-                    
-for(var cc=0;cc<counter;cc++){
- 
-
-    mem.push($('#c'+cc).html());
-    
-}
-for(var j=0;j<mem.length;j++){
-if(mem[j]!=undefined)
-    mem2.push(mem[j]);
-}
-console.log(mem2.length);
-console.log("mem2 : " +mem2);
 
 
-                
+
+                for (var cc = 0; cc < counter; cc++) {
+
+
+                    mem.push($('#c' + cc).html());
+
+                }
+                for (var j = 0; j < mem.length; j++) {
+                    if (mem[j] != undefined)
+                        mem2.push(mem[j]);
+                }
+                console.log(mem2.length);
+                console.log("mem2 : " + mem2);
+
+
+
                 var id = $(".input1").val();;
                 var name = $(".input2").val();
                 var bio = $(".textarea").val();
                 var user = $(".textarea1").val();
-           
+
                 var token = window.localStorage.getItem('token');
-              
-                
+
+
                 var settings = {
                     "url": "http://localhost:8000/groups/",
                     "method": "POST",
@@ -78,7 +79,7 @@ console.log("mem2 : " +mem2);
                     },
                     "headers": {
                         'X-CSRFToken': csrftoken,
-                       "accept": "application/json",
+                        "accept": "application/json",
                         "Access-Control-Allow-Origin": "*",
                         "Access-Control-Allow-Headers": "*",
                         "Content-Type": "application/json"
@@ -90,12 +91,12 @@ console.log("mem2 : " +mem2);
                         "invite_only": true,
                         "members": mem2,
                     }
-),
+                    ),
                 };
-             
+
                 $.ajax(settings).done(function (response) {
                     console.log(response);
-      
+
                     if (response.status === 400) {
                         console.log("no");
                     }
@@ -111,49 +112,48 @@ console.log("mem2 : " +mem2);
                 //    window.location.replace("/account/menu/");
                 // Window.location="/account/menu/"
 
-               });
-       
-  
+            });
 
+            
             $(".btn").click(function () {
                 var member = $(".input3").val();;
-          
+
                 //console.log(id + " " + name + " " + bio);
                 //console.log(csrftoken)
                 var form = new FormData();
                 var settings = {
-                    "url": "http://127.0.0.1:8000/user/"+member,
+                    "url": "http://127.0.0.1:8000/user/" + member,
                     "method": "GET",
                     "timeout": 0,
                     "processData": false,
                     "mimeType": "multipart/form-data",
                     "contentType": false,
                     "data": form,
-                    error:function(){
+                    error: function () {
                         alert("User not found");
                     }
-                  };
-                  
-                  $.ajax(settings).done(function (response) {
+                };
+
+                $.ajax(settings).done(function (response) {
                     console.log(response);
-                  
+
                     if (response.status === 400) {
                         alert("User not found");
                     }
                     else {
-                        htmlcode='';
-                        htmlcode += '<p className="pp" id=' + '"c' + counter + '">' + member+'</p>';
-                        var s="document.getElementById('close"+counter+"')";
-                        var ss=s+".remove()";
-                        var a="document.getElementById('c"+counter+"')";
-                        var aa=a+".remove()";
-                    
-                        var d="document.getElementById('h"+counter+"')";
-                        var dd=d+".remove()";
-                    
+                        htmlcode = '';
+                        htmlcode += '<p className="pp" id=' + '"c' + counter + '">' + member + '</p>';
+                        var s = "document.getElementById('close" + counter + "')";
+                        var ss = s + ".remove()";
+                        var a = "document.getElementById('c" + counter + "')";
+                        var aa = a + ".remove()";
 
-                        htmlcode += '<span onclick="'+ss+','+aa+','+dd+'"class="closes" id="close' + counter + '">&times;</span>';
-                        
+                        var d = "document.getElementById('h" + counter + "')";
+                        var dd = d + ".remove()";
+
+
+                        htmlcode += '<span onclick="' + ss + ',' + aa + ',' + dd + '"class="closes" id="close' + counter + '">&times;</span>';
+
                         htmlcode += '<hr class="line" id=' + '"h' + counter + '">';
                         $('.members').append(htmlcode);
                         counter++;
@@ -167,61 +167,63 @@ console.log("mem2 : " +mem2);
             });
         });
         $(document).ready(function () {
-        
+
         })
     };
-    
 
- 
+
+
     render() {
         return (
             <form className="form" >
                 <div id="homepagebtn">
                     <p>Homepage</p>
                 </div>
-            
+
                 <div className="formback">
                     <legend className="title">Create new group</legend>
-                <input  type="text"
-                    className="input1" placeholder="name" style={{
-                    height: '40px',
-                        width: '290px',
-                        marginLeft:'-30px'
-                    }} />
-                <input  type="text"
-                    className="input2" placeholder="id" style={{
-                    height: '40px',
-                    width: '290px'
-                    }} />
-                    <div classname ="search">
+                    <input type="text"
+                        className="input1" placeholder="name" style={{
+                            height: '40px',
+                            width: '290px',
+                            marginLeft: '-30px'
+                        }} />
+                    <input type="text"
+                        className="input2" placeholder="id" style={{
+                            height: '40px',
+                            width: '290px'
+                        }} />
+                    <div classname="search">
 
-                     <input   className="input3"  style={{
-                        height: '40px',
-                        width: '225px'
-                            }} placeholder="id of member  "/>
-          
+                        <input className="input3" style={{
+                            height: '40px',
+                            width: '225px'
+                        }} placeholder="id of member  " />
+
                         <div id="btn" className="btn" >add member</div>
 
                     </div>
-                   
-                    
-
-                  
-                <textarea  type="text"
-                    className="textarea" placeholder=" bio" style={{
-                    height: '150px',
-                    width: '290px'
-                    }} />
-                <div className='members' style={{width: '290px'}}>Members : <hr></hr></div>
-                <div id="create" className="button" variant="raised">next</div>
 
 
-                
+
+
+                    <textarea type="text"
+                        className="textarea" placeholder=" bio" style={{
+                            height: '50px',
+                            width: '290px'
+                        }} />
+                    <div className='members' style={{ maxHeight: '200px' , overflow: 'scroll', width: '290px' }}>Members : <hr></hr></div>
+
+
+                    <div id="create" className="button" variant="raised">next</div>
+
+
+
                 </div>
 
             </form>
 
-            )
+        )
     }
 
 }
