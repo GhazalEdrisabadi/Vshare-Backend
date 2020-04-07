06740fdfd2b813ed2b7106039b6d6f835a503275
Ghazaal <gahzal_edrisabadi@comp.iust.ac.ir>
2020-04-07 18:19:16 +0430

    Modify settings.py

--- vshare/vshare/settings.py
+++ vshare/vshare/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 # Application definition
 
+
 INSTALLED_APPS = [
 	'channels',
     'django.contrib.admin',
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework', # new
+    'rest_framework',
     'rest_framework.authtoken',
     'groups.apps.GroupsConfig',
     'users',
@@ -132,7 +133,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'vshare.wsgi.application'
-ASGI_APPLICATION = 'vshare.routing.application'
 
 
 # Database
@@ -149,26 +149,18 @@ DATABASES = {
     }
 }
 
+redis_host = os.environ.get('REDIS_HOST', 'localhost')
+
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'asgi_redis.RedisChannelLayer',
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('localhost', 6379)],
+            'hosts': [(redis_host, 6379)],
         },
-        'ROUTING': 'vshare.routing.channel_routing',
     }
 }
 
-# CHANNEL_LAYERS = {
-# 	'default': {
-# 		'BACKEND': 'channels_redis.core.RedisChannelLayer',
-# 		'CONFIG': {
-# 			"hosts": [CHANNEL_REDIS_HOST],
-# 			"symmetric_encryption_keys": [SECRET_KEY],
-#         },
-#     },
-# }
-
+ASGI_APPLICATION = 'vshare.routing.application'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -215,3 +207,31 @@ STATIC_URL = '/static/'
 
 MEDIA_URL =  '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR , 'users/media')
+
+
+NOTIFY_USERS_ON_ENTER_OR_LEAVE_ROOMS = True
+
+MSG_TYPE_MESSAGE = 0  # For standard messages
+MSG_TYPE_WARNING = 1  # For yellow messages
+MSG_TYPE_ALERT = 2  # For red & dangerous alerts
+MSG_TYPE_MUTED = 3  # For just OK information that doesn't bother users
+MSG_TYPE_ENTER = 4  # For just OK information that doesn't bother users
+MSG_TYPE_LEAVE = 5  # For just OK information that doesn't bother users
+
+MESSAGE_TYPES_CHOICES = (
+    (MSG_TYPE_MESSAGE, 'MESSAGE'),
+    (MSG_TYPE_WARNING, 'WARNING'),
+    (MSG_TYPE_ALERT, 'ALERT'),
+    (MSG_TYPE_MUTED, 'MUTED'),
+    (MSG_TYPE_ENTER, 'ENTER'),
+    (MSG_TYPE_LEAVE, 'LEAVE'),
+)
+
+MESSAGE_TYPES_LIST = [
+    MSG_TYPE_MESSAGE,
+    MSG_TYPE_WARNING,
+    MSG_TYPE_ALERT,
+    MSG_TYPE_MUTED,
+    MSG_TYPE_ENTER,
+    MSG_TYPE_LEAVE,
+]
