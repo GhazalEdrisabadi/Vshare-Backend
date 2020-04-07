2c855730154d075c00ee715e80b870be4831b0bb
Ghazaal <gahzal_edrisabadi@comp.iust.ac.ir>
2020-04-07 18:07:22 +0430

    Add state field to group model

--- vshare/groups/models.py
+++ vshare/groups/models.py
@@ -7,30 +7,59 @@ from pygments.styles import get_all_styles
 from django.conf import settings
 from django.core.validators import RegexValidator
 from django import forms
-# from multiselectfield import MultiSelectField
 from django.contrib.postgres.fields import ArrayField
+from django.apps import apps
+from enum import Enum 
 alphanumeric = RegexValidator(r'^[0-9a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
 
 class Group(models.Model):
-    since = models.DateTimeField(auto_now_add=True)
-    groupid = models.CharField(max_length=20, blank=False, null=False, validators=[alphanumeric], unique=True, default='')
-    #groupid only contains alphanumerical characters
-    title = models.CharField(max_length=100, blank=True, default='No Name')
-    describtion = models.TextField(blank=True)
-    invite_only = models.BooleanField(default=False)
-    created_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        verbose_name='Created by',
-        blank=True, null=True,
-        related_name="%(app_label)s_%(class)s_created",
-        on_delete=models.SET_NULL)
-    members = models.ManyToManyField(settings.AUTH_USER_MODEL,blank=True)
-    #upper field should be modified. because right now, it's pointing to django's default superuser model
-    
-    
-    class Meta:
-        ordering = ['since']
-    
+	since = models.DateTimeField(auto_now_add=True)
+	groupid = models.CharField(max_length=20, blank=False, null=False, validators=[alphanumeric], unique=True, default='')
+	#groupid only contains alphanumerical characters
+	title = models.CharField(max_length=100, blank=True, default='No Name')
+	describtion = models.TextField(blank=True)
+	invite_only = models.BooleanField(default=False)
+	created_by = models.ForeignKey(
+	    settings.AUTH_USER_MODEL,
+	    verbose_name='Created by',
+	    blank=True, null=True,
+	    related_name="%(app_label)s_%(class)s_created",
+	    on_delete=models.SET_NULL)
+	members = models.ManyToManyField(settings.AUTH_USER_MODEL,blank=True)
+	#upper field should be modified. because right now, it's pointing to django's default superuser model
+	
+	class STATUS(Enum):
+		initial = (0, 'no action')
+		selected = (1, 'video selected by owner')
+		validation = (2, 'check validation')
+		played = (3, 'video played by owner')
 
-    def __str__(self):
-        return self.title
+		@classmethod
+		def get_value(cls, member):
+			return member.value[0]
+
+	status = models.CharField(
+		max_length=32,
+		choices=[x.value for x in STATUS],
+		default=STATUS.get_value(STATUS.initial)
+	)
+    
+	class Meta:
+		ordering = ['since']
+
+
+	def __str__(self):
+		return self.title
+
+	def is_member(self,user):
+		UserModel = apps.get_model('users', 'Account')
+		user = UserModel.objects.get(username=username)
+		if user in self.members:
+			return True
+		else:
+			return False
+
+	#Return a unique channels.Group for each group through groupid
+	@property
+	def group_id(self):
+		return "room-%s" % self.groupid
