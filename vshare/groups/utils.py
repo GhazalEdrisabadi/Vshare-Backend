3df7dfbcfee858b2aa7e2ce10c26718d6bdee206
Ghazaal <gahzal_edrisabadi@comp.iust.ac.ir>
2020-04-11 20:31:34 +0430

    Add a new function to handle error

--- vshare/groups/utils.py
+++ vshare/groups/utils.py
@@ -5,14 +5,19 @@ from .models import *
 @database_sync_to_async
 def get_room_or_error(room_id, user):
 
+	if not user.is_authenticated:
+		raise ClientError("USER_HAS_TO_LOGIN")
 
-    if not user.is_authenticated:
-        raise ClientError("USER_HAS_TO_LOGIN")
+	# Find the room they requested (by ID)
+	try:
+		room = Group.objects.get(groupid=room_id)
+	except Group.DoesNotExist:
+		raise ClientError("ROOM_INVALID")
 
-# Find the room they requested (by ID)
-    try:
-        room = Group.objects.get(groupid=groupid)
-    except Group.DoesNotExist:
-        raise ClientError("ROOM_INVALID")
+	return room
 
-    return room
+@database_sync_to_async
+def join_error(user):
+	raise ClientError("USER_IS_NOW_A_MEMBER")
+
+
