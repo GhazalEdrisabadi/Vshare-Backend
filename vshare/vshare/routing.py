b85c2801964dc8b094993350ddf11e99e7321c77
Ghazaal <gahzal_edrisabadi@comp.iust.ac.ir>
2020-04-15 22:05:30 +0430

    Create new middlware and apply changes in routing

--- vshare/vshare/routing.py
+++ vshare/vshare/routing.py
@@ -1,17 +1,21 @@
-from django.urls import path
 from django.conf.urls import url
-
 from channels.http import AsgiHandler
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from groups.utils import TokenAuthMiddlewareStack
+
+
+from groups.middlewares import TokenAuthMiddleware
 from groups.consumers import VideoConsumer
 
-
 application = ProtocolTypeRouter({
-	"websocket":TokenAuthMiddlewareStack(
-		URLRouter([
-			url(r'^groups/stream', VideoConsumer),
-		])
+	"websocket":TokenAuthMiddleware(
+		URLRouter(
+			[
+				# url(r'^stream/groups/(?P<groupid>[\w.@+-]+/$', VideoConsumer),
+				url(r'^stream/groups', VideoConsumer),
+			]
+		)
 	)
 })
+
+# ws://ourdomain/<groupid>/?token=token
