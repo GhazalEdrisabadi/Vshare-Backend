cd9bc75b826c7c59f2c6d7e3e8691f616c7915a6
kamyab <amirhosseinghassemi78@yahoo.com>
2020-04-15 16:52:59 +0430

    Fix scroll

--- vshare/src/Strartpage/Startpage.js
+++ vshare/src/Strartpage/Startpage.js
@@ -9,12 +9,12 @@ import ReactDOM from 'react-dom';
 class startpage extends Component {
     render() {
         return (
-       
+
             <div className="back_startpage">
                 <div className="Main_div">
                 <header className="page-header" >
 
-                    <div className="div_header_start">
+                        <div className="div_header_start" style={{ overflow:"hidden" }}>
                         <div className="div_site_name">
                             <h1 className="site_name">Vshare</h1>
                         </div>
@@ -45,8 +45,10 @@ class startpage extends Component {
                         <button className="btn_start"><a href="./login">Get Start</a></button>
                     </div>
           
+                    </div>
+                
                 </div>
-            </div>
+            
             )
 
     }
