6495644c93793fc1de0757a31876228c5adac0fb
kamyab <amirhosseinghassemi78@yahoo.com>
2020-04-22 06:07:57 +0430

    Show video when select and click play

--- vshare/src/chatroom/chat_room.js
+++ vshare/src/chatroom/chat_room.js
@@ -3,7 +3,11 @@ import './chat_room.css'
 import $ from 'jquery';
 import Websocket from 'react-websocket';
 import Home from './home.png'
+import ReactPlayer from 'react-player'
+import { object } from 'prop-types';
+var ass=0;
 class chat_room extends Component {
+ 
     componentDidMount() {
         if (window.localStorage.getItem('token') == null) {
 
@@ -13,16 +17,19 @@ class chat_room extends Component {
 
         }
         var id_gp = window.localStorage.getItem('id_gp');
-        //id_gp = "test";
+        id_gp = "test";
         //This will open the connection*
         var ws = new WebSocket("ws://127.0.0.1:8000/stream/groups/" + id_gp + "/?token=" + localStorage.getItem('token') + "");
         ws.onopen = function () {
             console.log("Ping");
         };
-
+   
         const { id } = this.props.match.params
         $(document).ready(function () {
-
+            if (ass == 1) {
+                console.log("shoeeeeeeeee")
+                $("#movie").fadeIn();
+            }
 
             // if (localStorage.getItem('token') == null) {
             //     alert("Login please !");
@@ -30,8 +37,8 @@ class chat_room extends Component {
             // }
         
                 var id = window.localStorage.getItem('id_gp');
-                var settings = {
-                    "url": "http://127.0.0.1:8000/groups/"+id+'/',
+            var settings = {
+                "url": "http://127.0.0.1:8000/groups/" + id_gp + '/',
                     "method": "GET",
                     "timeout": 0,
                     "headers": {
@@ -68,9 +75,37 @@ class chat_room extends Component {
 
 
         //Log the messages that are returned from the server
-  
+     
     }
+   
+    constructor(props) {
+        super(props)
+        this.state = {
+            marhale:0,
+            file_select: null,
+            file_show_when_click: null
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event) {
+        this.setState({
+            file_select: URL.createObjectURL(event.target.files[0])
+        })
+    }
+    handleSubmit(e) {
+    
+        this.setState({
+            file_show_when_click: this.state.file_select
+        })
+        console.log("onchange")
+        document.getElementById('movie').style.display = 'block';
+    }
+  
     render() {
+        
+        
+      //  const url = URL.createObjectURL(ass)
         return (
             <div>
                 
@@ -83,8 +118,11 @@ class chat_room extends Component {
                             <div className="name"/>
                         </div>
                     </header>
-                <div className="formback_movie">
+                    <div className="formback_movie">
+                        <input type="file" onChange={this.handleChange} />
+                        <div onClick={this.handleSubmit}>Play</div>
 
+                        <ReactPlayer id="movie" className="video" url={this.state.file_show_when_click} playing />
                 </div>
                 <div className="back_coulom">
 
