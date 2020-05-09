312acd9a6e6f50c0489e38c79fb07158f7b99f32
Ghazaal <gahzal_edrisabadi@comp.iust.ac.ir>
2020-05-09 19:08:54 +0430

    Add url to edit profile

--- vshare/users/urls.py
+++ vshare/users/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('signup/', Registration.as_view(), name="Signup"),
     path('login/', UserLogin.as_view(), name="Login"),
     path('<str:username>/', UserByUsername.as_view(), name="detail"),
-
+    path('<str:username>/edit/', EditProfile.as_view(), name="Edit Profile")
 ]
