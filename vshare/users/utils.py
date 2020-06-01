6bc02c3e23b31a97ff9ae5de5535f60fb2d3341d
Ghazaal <gahzal_edrisabadi@comp.iust.ac.ir>
2020-06-01 21:17:09 +0430

    Add two defs to generate upload and get photo url

--- vshare/users/utils.py
+++ vshare/users/utils.py
@@ -1,26 +1,13 @@
 import logging
 import boto3
 from botocore.exceptions import ClientError
-
-
+from django.conf import settings
 
 
 def create_presigned_url(bucket_name, object_name, expiration=3600):
-    """Generate a presigned URL to share an S3 object
 
-    :param bucket_name: string
-    :param object_name: string
-    :param expiration: Time in seconds for the presigned URL to remain valid
-    :return: Presigned URL as string. If error, returns None.
-    """
+    s3_client = boto3.client('s3')
 
-    # Generate a presigned URL for the S3 object
-    s3_client = boto3.client(
-    's3',
-    endpoint_url = "http://localhost:9000",
-    aws_access_key_id='minio',
-    aws_secret_access_key='miniostorage',
-    )
     try:
         response = s3_client.generate_presigned_url('get_object',
                                                     Params={'Bucket': bucket_name,
@@ -30,32 +17,13 @@ def create_presigned_url(bucket_name, object_name, expiration=3600):
         logging.error(e)
         return None
 
-    # The response contains the presigned URL
     return response
 
 
 def create_presigned_post(bucket_name, object_name,
                           fields=None, conditions=None, expiration=3600):
-    """Generate a presigned URL S3 POST request to upload a file
-
-    :param bucket_name: string
-    :param object_name: string
-    :param fields: Dictionary of prefilled form fields
-    :param conditions: List of conditions to include in the policy
-    :param expiration: Time in seconds for the presigned URL to remain valid
-    :return: Dictionary with the following keys:
-        url: URL to post to
-        fields: Dictionary of form fields and values to submit with the POST
-    :return: None if error.
-    """
-
-    # Generate a presigned S3 POST URL
-    s3_client = boto3.client(
-    's3',
-    endpoint_url = "http://localhost:9000",
-    aws_access_key_id='minio',
-    aws_secret_access_key='miniostorage',
-    )
+   
+    s3_client = boto3.client('s3')
 
     try:
         response = s3_client.generate_presigned_post(bucket_name,
@@ -67,5 +35,4 @@ def create_presigned_post(bucket_name, object_name,
         logging.error(e)
         return None
 
-    # The response contains the presigned URL and required fields
     return response
