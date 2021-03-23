9ec0d0388da1ffe18e74af40eed0719d00cc0db6
Ghazaal <gahzal_edrisabadi@comp.iust.ac.ir>
2021-03-23 15:06:13 +0430

    Add jwt tokens def in account class

--- vshare/users/models.py
+++ vshare/users/models.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class MyAccountManager(BaseUserManager):
 
@@ -67,10 +68,13 @@ class Account(AbstractBaseUser):
 	def has_module_perms(self, app_label):
 		return True
 
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-	if created:
-		Token.objects.create(user=instance)
+	def tokens(self):
+		refresh = RefreshToken.for_user(self)
+		return {
+			'refresh' : str(refresh),
+			'access' : str(refresh.access_token)
+		}
+
 
 class Friendship(models.Model):
 	who_follows = models.ForeignKey(settings.AUTH_USER_MODEL,to_field='username',blank=True,null=True,on_delete=models.CASCADE,related_name="top")
