ab5fabdd7b5cf3c8532e84ef0b12e69dd3eefa1d
amin7799sh <amin7799sh@gmail.com>
2021-03-25 19:09:39 +0430

    Add an endpoint for google social accounts.

--- vshare/vshare/urls.py
+++ vshare/vshare/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('users.urls')),
     path('', include('groups.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
 
 if settings.DEBUG:
