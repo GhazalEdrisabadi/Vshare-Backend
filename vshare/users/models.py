480cee6d2ca13bf4a241045a6a881fb726d96043
amin7799sh <amin7799sh@gmail.com>
2021-03-27 12:36:08 +0430

    Change adapter for social account.

--- vshare/users/models.py
+++ vshare/users/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager , PermissionsMixin
-
+from django.contrib.auth.models import User
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.dispatch import receiver
+from allauth.account.signals import user_signed_up
 
 class MyAccountManager(BaseUserManager):
 
@@ -76,7 +78,30 @@ class Account(AbstractBaseUser,PermissionsMixin):
 			'access' : str(refresh.access_token)
 		}
 
+#When a user logs in with google account, beside a social account 
+#instance, a custom user model instance will be alse created, this
+#function populates this instance of custom user model.
+'''
+	@receiver(user_signed_up)
+	def populate_profile(sociallogin, user, **kwargs):
+		user.profile = Account()
 
+		if sociallogin.account.provider == 'google':
+			user_data = user.socialaccount_set.filter(provider='google')[0].extra_data
+			#picture_url = user_data['picture']
+			#email = user_data['email']
+			first_name = user_data['given_name']
+			last_name = user_data['family_name']
+
+		#user.profile.photo = picture_url
+		user.profile.email = '12345@yahoo.com'
+		user.profile.firstname = first_name
+		user.profile.lastname = last_name
+		user.profile.username = '121212'
+		print(last_name)
+		print('//////////////////////////////////////////////////////////////')
+		user.profile.save()
+'''
 class Friendship(models.Model):
 	who_follows = models.ForeignKey(settings.AUTH_USER_MODEL,to_field='username',blank=True,null=True,on_delete=models.CASCADE,related_name="top")
 	who_is_followed = models.ForeignKey(settings.AUTH_USER_MODEL,to_field='username',blank=True,null=True,on_delete=models.CASCADE,related_name="bot")
