31acfcdace56af3b5e54cf0f9a8f24cd784f9950
Ghazaal <gahzal_edrisabadi@comp.iust.ac.ir>
2021-04-12 23:34:10 +0430

    Change users url

--- vshare/vshare/urls.py
+++ vshare/vshare/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('users.urls')),
+    path('users/', include('users.urls')),
     path('', include('groups.urls')),
     path('accounts/', include('allauth.urls')),
 ]
