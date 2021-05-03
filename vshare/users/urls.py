c767f57d88c6d4f593bb632a875e0f669c7cc92d
Ghazaal <gahzal_edrisabadi@comp.iust.ac.ir>
2021-05-03 15:47:54 +0430

    Add follow-request-list endpoint

--- vshare/users/urls.py
+++ vshare/users/urls.py
@@ -31,4 +31,5 @@ urlpatterns = [
     path('auth/login/',LoginView.as_view(),name='account_login'),
     path('relations/online_followings/',UserOnlineFollowings,name='online_followings'),
     path('relations/offline_followings/',UserOfflineFollowings,name='offline_followings'),
+    path('relations/follow-requests/',FriendRequestList.as_view(),name='friend_requests'),
     ]
