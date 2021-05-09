f7ab8fc68fae267f7bbc9f919c8c5c676f7ca737
amin7799sh <amin7799sh@gmail.com>
2021-05-10 01:20:04 +0430

    Create new pagination class for Dm.

--- vshare/groups/pagination.py
+++ vshare/groups/pagination.py
@@ -6,9 +6,26 @@ from rest_framework.response import Response
 
 DEFAULT_PAGE = 1
 
-class CustomPagination(PageNumberPagination):
+class MessageCustomPagination(PageNumberPagination):
     page = DEFAULT_PAGE
     page_size = 100
+    page_size_query_param = 'page_size'
+
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'total': self.page.paginator.count,
+            'page': int(self.request.GET.get('page', DEFAULT_PAGE)),
+            'page_size': int(self.request.GET.get('page_size', self.page_size)),
+            'results': data
+        })
+
+class DirectMessageCustomPagination(PageNumberPagination):
+    page = DEFAULT_PAGE
+    page_size = 20
     page_size_query_param = 'page_size'
 
     def get_paginated_response(self, data):
