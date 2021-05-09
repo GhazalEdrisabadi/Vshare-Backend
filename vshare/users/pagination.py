0da88e0113a6e250318ff4926cacc56a1924c912
amin7799sh <amin7799sh@gmail.com>
2021-05-10 01:19:25 +0430

    Generate new migrations fo Users app.

---
+++ vshare/users/pagination.py
@@ -0,0 +1,21 @@
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+DEFAULT_PAGE = 1
+
+class DirectMessageCustomPagination(PageNumberPagination):
+    page = DEFAULT_PAGE
+    page_size = 20
+    page_size_query_param = 'page_size'
+
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'total': self.page.paginator.count,
+            'page': int(self.request.GET.get('page', DEFAULT_PAGE)),
+            'page_size': int(self.request.GET.get('page_size', self.page_size)),
+            'results': data
+        })
