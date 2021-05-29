618fa358cfd1f05e022f3382bdf1e75e5de8567a
Ghazaal <gahzal_edrisabadi@comp.iust.ac.ir>
2021-05-29 22:04:23 +0430

    Add jwt middleware

--- vshare/stream/middlewares.py
+++ vshare/stream/middlewares.py
@@ -1,3 +1,6 @@
+"""
+------------- Token Authentication middleware ---------------
+
 from rest_framework.authtoken.models import Token
 from django.conf import settings
 from channels.db import database_sync_to_async
@@ -36,5 +39,270 @@ class TokenAuthMiddleware:
 
 TokenAuthMiddlewareStack = lambda inner: TokenAuthMiddleware(AuthMiddlewareStack(inner))
 
+---------------------------------------------------------------------
+
+"""
+
+#------------------- JWT Authentication middleware ---------------------
+from django.db import close_old_connections
+from rest_framework_simplejwt.tokens import UntypedToken
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from channels.auth import AuthMiddlewareStack
+from channels.db import database_sync_to_async
+from django.contrib.auth import get_user_model
+from urllib.parse import parse_qs
+from jwt import decode as jwt_decode
+from django.conf import settings
+from django.contrib.auth.models import AnonymousUser
+import asyncio
+
+
+User = get_user_model()
+
+@database_sync_to_async
+def get_user(user_id):
+	try:
+		return User.objects.get(username=user_id)
+	except User.DoesNotExist:
+		return AnonymousUser()
 		
+class JWTAuthMiddleware:
+		
+	def __init__(self, app):
+		# Store the ASGI application we were passed
+		self.app = app
+		
+	async def __call__(self, scope, receive, send):
+
+		# Close old database connections to prevent usage of timed out connections
+		close_old_connections()
+
+		# Get the token
+		token = parse_qs(scope["query_string"].decode("utf8"))["token"][0]
+
+		# Try to authenticate the user
+		try:
+
+			# This will automatically validate the token and raise an error if token is invalid
+			UntypedToken(token)
+
+		except (InvalidToken, TokenError) as e:
+
+			# Token is invalid
+			print(e)
+			return None
+
+		else:
+
+			#  Then token is valid, decode it
+			decoded_data = jwt_decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+			# Will return a dictionary like -
+			# {
+			#     "token_type": "access",
+			#     "exp": 1568770772,
+			#     "jti": "5c15e80d65b04c20ad34d77b6703251b",
+			#     "user_id": 6
+			# }
+
+			# Get the user using ID
+			scope['user'] = await get_user(decoded_data["user_id"])
+		
+		return await self.app(scope, receive, send)
+
+JWTAuthMiddlewareStack = lambda inner: JWTAuthMiddleware(AuthMiddlewareStack(inner))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from channels.db import database_sync_to_async
+# from django.contrib.auth import get_user_model
+# from django.contrib.auth.models import AnonymousUser
+# from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+# from rest_framework_simplejwt.tokens import UntypedToken
+# from rest_framework_simplejwt.authentication import JWTTokenUserAuthentication
+# from rest_framework_simplejwt.state import User
+# from channels.middleware import BaseMiddleware
+# from channels.auth import AuthMiddlewareStack
+# from django.db import close_old_connections
+# from urllib.parse import parse_qs
+# from jwt import decode as jwt_decode
+# from django.conf import settings
+
+# @database_sync_to_async
+# def get_user(validated_token):
+#     try:
+#         user = get_user_model().objects.get(username=validated_token["user_id"])
+#         return user
+   
+#     except Account.DoesNotExist:
+#         return AnonymousUser()
+
+# class JWTAuthMiddleware:
+#     """
+#     Custom middleware (insecure) that takes user IDs from the query string.
+#     """
+
+#     def __init__(self, app):
+#         # Store the ASGI application we were passed
+#         self.app = app
+
+#     async def __call__(self, scope, receive, send):
+#         # Look up user from query string (you should also do things like
+#         # checking if it is a valid user ID, or if scope["user"] is already
+#         # populated).
+#                # Close old database connections to prevent usage of timed out connections
+#         close_old_connections()
+
+#         # Get the token
+#         token = parse_qs(scope["query_string"].decode("utf8"))["token"][0]
+
+#         # Try to authenticate the user
+#         try:
+#             # This will automatically validate the token and raise an error if token is invalid
+#             UntypedToken(token)
+#         except (InvalidToken, TokenError) as e:
+#             # Token is invalid
+#             print(e)
+#             return None
+#         else:
+#             #  Then token is valid, decode it
+#             decoded_data = jwt_decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+#             print(decoded_data)
+#             # Will return a dictionary like -
+#             # {
+#             #     "token_type": "access",
+#             #     "exp": 1568770772,
+#             #     "jti": "5c15e80d65b04c20ad34d77b6703251b",
+#             #     "user_id": 6
+#             # }
+
+#             # Get the user using ID
+#             scope["user"] = await get_user(validated_token=decoded_data)
+
+#         return await self.app(scope, receive, send)
+
+# from channels.db import database_sync_to_async
+# from django.contrib.auth import get_user_model
+# from django.contrib.auth.models import AnonymousUser
+# from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+# from rest_framework_simplejwt.tokens import UntypedToken
+# from rest_framework_simplejwt.authentication import JWTTokenUserAuthentication
+# from rest_framework_simplejwt.state import User
+# from channels.middleware import BaseMiddleware
+# from channels.auth import AuthMiddlewareStack
+# from django.db import close_old_connections
+# from urllib.parse import parse_qs
+# from jwt import decode as jwt_decode
+# from django.conf import settings
+
+# @database_sync_to_async
+# def get_user(validated_token):
+#     try:
+#         user = get_user_model().objects.get(username=validated_token["user_id"])
+#         # return get_user_model().objects.get(id=toke_id)
+#         print(f"{user}")
+#         return user
+   
+#     except User.DoesNotExist:
+#         return AnonymousUser()
+
+
+
+# class JwtAuthMiddleware(BaseMiddleware):
+#     def __init__(self, inner):
+#         self.inner = inner
+
+#     async def __call__(self, scope, receive, send):
+#        # Close old database connections to prevent usage of timed out connections
+#         close_old_connections()
+
+#         # Get the token
+#         token = parse_qs(scope["query_string"].decode("utf8"))["token"][0]
+
+#         # Try to authenticate the user
+#         try:
+#             # This will automatically validate the token and raise an error if token is invalid
+#             UntypedToken(token)
+#         except (InvalidToken, TokenError) as e:
+#             # Token is invalid
+#             print(e)
+#             return None
+#         else:
+#             #  Then token is valid, decode it
+#             decoded_data = jwt_decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+#             print(decoded_data)
+#             # Will return a dictionary like -
+#             # {
+#             #     "token_type": "access",
+#             #     "exp": 1568770772,
+#             #     "jti": "5c15e80d65b04c20ad34d77b6703251b",
+#             #     "user_id": 6
+#             # }
+
+#             # Get the user using ID
+#             scope["user"] = await get_user(validated_token=decoded_data)
+#         return await super().__call__(scope, receive, send)
+
+
+# def JwtAuthMiddlewareStack(inner):
+#     return JwtAuthMiddleware(AuthMiddlewareStack(inner))
 	
